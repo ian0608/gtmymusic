e649f5ff9b502ec8790766bcedcf6f46304ea780
ian0608 <ian0608@users.noreply.github.com>
2013-10-06 14:04:15 -0400

    Added diff functionality

--- utils.c
+++ utils.c
@@ -6,8 +6,6 @@
 
 /*
 Requires linking with -lcrypto
-
-This function returns a pointer to a list_item_array representing the mp3 files in the current directory. Each individual list_item contains the filename and MD5 hash of the file contents. See main for an example.
 */
 
 int init_list_item_array(list_item_array **toInit)
@@ -44,6 +42,10 @@ int incr_size_list_item_array(list_item_array **toIncr)
 	}
 	return 0;
 }
+
+/*
+This function returns a pointer to a list_item_array representing the mp3 files in the current directory. Each individual list_item contains the filename and MD5 hash of the file contents. See main for an example.
+*/
 
 list_item_array *get_list_items_current_dir()
 {
@@ -121,6 +123,40 @@ void teardown_list_item_array(list_item_array *item_array)
 	free(item_array);
 }
 
+/*
+This function returns a pointer to a list_item_array containing the hash/filename pairs contained in "authoritative" but not in "other." See main for an example.
+*/
+
+list_item_array *diff_lists(list_item_array *authoritative, list_item_array *other)
+{
+	list_item_array *diff_list;
+	if(init_list_item_array(&diff_list) < 0)
+		return NULL;
+
+	int i;
+	for (i=0; i < authoritative->count; i++)
+	{
+		int found = 0;
+		int j;
+		for (j=0; j < other->count; j++)
+		{
+			if(memcmp(authoritative->items[i]->hash, other->items[j]->hash, MD5_DIGEST_LENGTH) == 0)
+			{
+				found = 1;
+			}
+		}
+		if (found == 0)
+		{
+			if(incr_size_list_item_array(&diff_list) < 0)
+				return NULL;
+
+			memcpy(diff_list->items[diff_list->count-1]->hash, authoritative->items[i]->hash, MD5_DIGEST_LENGTH);
+			memcpy(diff_list->items[diff_list->count-1]->filename, authoritative->items[i]->filename, strlen(authoritative->items[i]->filename)+1);
+		}
+	}
+	return diff_list;
+}
+
 int main(int argc, char *argv[])
 {
 	list_item_array *myList = get_list_items_current_dir();
@@ -129,6 +165,7 @@ int main(int argc, char *argv[])
 		return -1;
 	}
 
+	printf("list creation test\n");
 	int i=0;
 	while (i < myList->count)	//for each list_item
 	{
@@ -140,7 +177,38 @@ int main(int argc, char *argv[])
 		i++;
 	}
 
+	printf("diff test\n");
+	list_item_array *myList2 = get_list_items_current_dir();
+	if (myList2 == NULL)
+	{
+		return -1;
+	}
+	if(incr_size_list_item_array(&myList2) < 0)
+		return -1;
+	char testFilename[] = "test.mp3";
+	char testHash[] = "abcdefg";
+	memcpy(myList2->items[myList2->count-1]->hash, testHash, 7);
+	memcpy(myList2->items[myList2->count-1]->filename, testFilename, strlen(testFilename)+1);
+	list_item_array *diff = diff_lists(myList2, myList);
+	if (diff == NULL)
+	{
+		return -1;
+	}
+	i=0;
+	while (i < diff->count)	//for each list_item
+	{
+		printf("%s\n", diff->items[i]->filename);	//print the filename
+		int j;
+		for (j=0; j < MD5_DIGEST_LENGTH; j++)				//and print the hash
+			printf("%02x", diff->items[i]->hash[j]);
+		printf("\n");
+		i++;
+	}
+
+
 	teardown_list_item_array(myList);
+	teardown_list_item_array(myList2);
+	teardown_list_item_array(diff);
 
 	return 0;
 }

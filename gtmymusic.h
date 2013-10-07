b57ccd24948d43c15f2d4d997c85f1714c3fa30b
ian0608 <ian0608@users.noreply.github.com>
2013-10-06 21:14:11 -0400

    Added utils function prototypes to header file

--- gtmymusic.h
+++ gtmymusic.h
@@ -10,3 +10,9 @@ typedef struct {
 	int count;
 	list_item **items;
 } list_item_array;
+
+int init_list_item_array(list_item_array **toInit);
+list_item_array *get_list_items_current_dir();
+void teardown_list_item_array(list_item_array *item_array);
+list_item_array *diff_lists(list_item_array *authoritative, list_item_array *other);
+

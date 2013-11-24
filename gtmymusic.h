039fdbb20703d96df70fdc45101b3edaa67e7ff0
ian0608 <ian0608@users.noreply.github.com>
2013-11-24 13:50:26 -0500

    Updated to submitted version of Project 2

---
+++ gtmymusic.h
@@ -0,0 +1,19 @@
+#include <openssl/md5.h>
+#include <dirent.h>
+
+typedef struct {
+	unsigned char hash[MD5_DIGEST_LENGTH];
+	char filename[NAME_MAX + 1];
+} list_item;
+
+typedef struct {
+	int count;
+	list_item **items;
+} list_item_array;
+
+int init_list_item_array(list_item_array **toInit);
+int incr_size_list_item_array(list_item_array **toIncr);
+list_item_array *get_list_items_current_dir();
+void teardown_list_item_array(list_item_array *item_array);
+list_item_array *diff_lists(list_item_array *authoritative, list_item_array *other);
+

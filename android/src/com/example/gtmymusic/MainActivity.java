10dfd33dabfc0b99c93dcb37eb62611ae6fd5a4f
ian0608 <ian0608@users.noreply.github.com>
2013-11-26 02:55:56 -0500

    LIST communication working

--- android/src/com/example/gtmymusic/MainActivity.java
+++ android/src/com/example/gtmymusic/MainActivity.java
@@ -6,7 +6,6 @@ import java.io.OutputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -45,7 +44,7 @@ public class MainActivity extends Activity {
 	{
 		protected String doInBackground(Void... params)
 		{
-			String toReturn = "";
+			StringBuilder builder = new StringBuilder();
 			//NETWORKING GOES HERE
         	try {
         		Socket s = new Socket("130.207.114.22", 6079);
@@ -67,13 +66,69 @@ public class MainActivity extends Activity {
                 //byte[] bytes = new byte[32];
                 byte[] countBytes = new byte[4];
                 int bytesRead = stream.read(countBytes, 0, 4);
+                if (bytesRead < 4)
+                {
+                	s.close();
+                	return "Error reading item count";
+                }
+                
                 ByteBuffer bb = ByteBuffer.wrap(countBytes);
-                bb.order(ByteOrder.BIG_ENDIAN);
+                //bb.order(ByteOrder.BIG_ENDIAN); default
                 int count = bb.getInt();
                 
                 //TextView text = (TextView)findViewById(R.id.message_text);
                 //text.setText(count + " items");
-                toReturn = bytesRead + " " + count;
+                //toReturn = bytesRead + " " + count;
+                
+                
+                
+                for (int i=0; i < count; i++)
+                {
+                	byte[] itemBytes = new byte[273];
+                	bytesRead = stream.read(itemBytes, 0, 273);
+                	if (bytesRead < 273)
+                	{
+                		s.close();
+                		return "Read unexpected number of item bytes";
+                	}
+                	
+                	for (int j=0; j<16; j++)
+                    {
+                    	//if (j==0 && itemBytes[j] < 16) builder.append("0");
+                    	//builder.append(Integer.toHexString((int) (itemBytes[j] & 0xff)));
+                		String hex = Integer.toHexString((itemBytes[j] & 0xff));
+                		if (hex.length() == 1)
+                			builder.append('0');
+                		builder.append(hex);
+                		
+                    }
+                	builder.append(", ");
+                	
+                	int nullTerm = -1;
+                	
+                	for (int k=16; k<273; k++)
+                	{
+                		if (itemBytes[k] == 0)
+                		{
+                			nullTerm = k;
+                			break;
+                		}
+                	}
+                	
+                	if (nullTerm == -1)
+                	{
+                		s.close();
+                		return "Error reading a filename";
+                	}
+
+                	//bb = ByteBuffer.wrap(itemBytes, 16, nullTerm-16);
+                	//builder.append(new String(bb.array()));
+                	byte[] filenameBytes = new byte[nullTerm-16];
+                	System.arraycopy(itemBytes, 16, filenameBytes, 0, nullTerm-16);
+                	builder.append(new String(filenameBytes));
+                	
+                	builder.append("\n");
+                }
                 
                 //Close connection
                 s.close();
@@ -86,7 +141,7 @@ public class MainActivity extends Activity {
         		e.printStackTrace();
         	}
 			
-			return toReturn;
+			return builder.toString();
 		}
 		
 		protected void onPostExecute(String result) {
